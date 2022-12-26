8518180d05f2d25701dadd37985a90a42027a926
Edye Quisca <edye.1230@gmail.com>
2022-12-26 12:43:03 -0500

    .

--- src/basicserver/BasicServer.java
+++ src/basicserver/BasicServer.java
@@ -17,7 +17,7 @@ public static void main(String[] args) throws Exception {
         System.out.println("Conexión aceptada desde " + clientSocket.getInetAddress().getHostAddress());
 
         // Obtiene los flujos de entrada y salida del socket de cliente
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
         // Lee la primera línea de la solicitud del cliente, que debería ser la línea de solicitud (por ejemplo, "GET /path HTTP/1.1")
@@ -32,10 +32,10 @@ public static void main(String[] args) throws Exception {
         // Si el verbo de solicitud es "GET" y la ruta es "/", escribe una respuesta al cliente
         if (method.equals("GET") && path.equals("/")) {
             out.println(protocol + " 200 OK");
-            out.println("Content-Type: text/html");
+            out.println("Content-Type: text/html;utf-8");
             out.println();
             out.println("<h1>Bienvenido al servidor</h1>");
-            out.println("<p>Esta es la página principal del servidor.</p>");
+            out.println("<p>Esta es la pagina principal del servidor.</p>");
         } else {
             // Si el verbo de solicitud o la ruta no son válidos, escribe una respuesta de error al cliente
             out.println(protocol + " 404 Not Found");
